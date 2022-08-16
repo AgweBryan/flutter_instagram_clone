624d4167f5f696b64e4f2c7e6da81f05eb6ff2e8
Agwe Bryan <agwedinobryan@gmail.com>
2022-08-16 22:22:47 +0100

    working on profile screen

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,5 +1,10 @@
+import 'package:flutter_instagram_clone/utils/constants.dart';
 import 'package:get/get.dart';
 
+import 'package:flutter_instagram_clone/models/user.dart' as model;
+
 class ProfileController extends GetxController {
-  // TODO: Sutff
+  final Rx<Map<String, dynamic>> _user = Rx<Map<String, dynamic>>({});
+
+  Map<String, dynamic> get user => _user.value;
 }
--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -1,21 +1,136 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/controllers/posts_controller.dart';
 import 'package:flutter_instagram_clone/controllers/profile_controller.dart';
 import 'package:flutter_instagram_clone/utils/colors.dart';
+import 'package:flutter_instagram_clone/utils/constants.dart';
 import 'package:get/get.dart';
 
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({Key? key}) : super(key: key);
   final ProfileController _profileController = Get.put(ProfileController());
+  final PostsController _postsController = Get.find();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
-        title: const Text(
-          'username',
+        title: Text(
+          authController.username.value,
         ),
       ),
-      body: const Center(child: Text('Profile Screen')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundColor: mobileBackgroundColor,
+                  backgroundImage: MemoryImage(
+                    authController.currentUserProfilePhoto.value,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        // mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _buildlStateColumn(22, 'posts'),
+                          _buildlStateColumn(150, 'followers'),
+                          _buildlStateColumn(10, 'following'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            child: _followButton(
+                              text: 'Sign Out',
+                              backgroundColor: mobileBackgroundColor,
+                              textColor: purpleColor,
+                              borderColor: Colors.grey,
+                              onTap: () {},
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildlStateColumn(int count, String label) {
+    return Column(
+      // mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          count.toString(),
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+
+  _followButton(
+      {required Color borderColor,
+      required String text,
+      required Color textColor,
+      required Color backgroundColor,
+      required Function() onTap}) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: TextButton(
+        onPressed: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            border: Border.all(
+              color: borderColor,
+            ),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          alignment: Alignment.center,
+          height: 29,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
